f7e468314632f1f1eafd8ceb61576556e8b706ec
agusputra <agus.putra99@gmail.com>
2018-12-11 09:19:16 +0700

    use express

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-var spawn = require('child_process').spawn;
+const spawn = require('child_process').spawn
+const express = require('express')
 
-spawn('twitter-proxy');
-spawn('http-server');
+spawn('twitter-proxy')
+spawn('http-server')
 
-console.log('Server running on http://localhost:8080');
-console.log('Request the Twitter API using: http://localhost:7890/1.1/statuses/user_timeline.json\?count\=30\&screen_name\=makeschool');
+const puts = console.log.bind(console)
+const app = express()
 
+console.log('Server running on http://localhost:8080')
+console.log('Request the Twitter API using: http://localhost:7890/1.1/statuses/user_timeline.json\?count\=30\&screen_name\=makeschool')
+
+app.use(express.static('build'));
+app.use((req, res, cb) => {
+  res.render = (filePath) => res.sendFile(`${__dirname}/${filePath}`);
+  cb()
+});
+
+app.get("/", (req, res) => {
+  res.render('index.html')
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  puts(`Node.js ${process.version} - Listening on port ${listener.address().port}`);
+});
--- src/ColumnHead.js
+++ src/ColumnHead.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ColumnHead(props) {
   return (
-    <div className="column-head card-header"><strong>Tweets by <a href={`https://twitter.com/${props.title}`}>{props.title}</a></strong></div>
+    <div className="column-head card-header"><strong>Tweets by <a href={`https://twitter.com/${props.title}`}>{props.title}</a></strong><small style={{position: 'absolute', right: 5, top: 5}}>(DRAG this)</small></div>
   )
 }
